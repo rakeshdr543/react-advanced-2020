9a8401995562fdd1400988a788a6cbf4d1cc0d49
rakesh <rakeshdr543@gmail.com>
2020-12-23 06:26:11 +0530

    Added multiple input form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import UseRefBasics1 from './tutorial/5-useRef/final/1-useRef-basics'
+import UseRefBasics from './tutorial/5-useRef/setup/1-useRef-basics'
+
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
+     <UseRefBasics />
+     <UseRefBasics1 />
+
+
     </div>
   )
 }

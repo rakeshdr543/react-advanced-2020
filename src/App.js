6bdd3e5cf7e3e1e685e5a64bdf5bacc91d5957cb
rakesh <rakeshdr543@gmail.com>
2020-12-24 07:16:22 +0530

    Added custom hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import Final from './tutorial/8-useContext/final/1-context-api'
-import Setup from './tutorial/8-useContext/setup/1-context-api'
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
 
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-     <Final />
      <Setup />
 
 
